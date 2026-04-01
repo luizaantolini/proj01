c6850bff93fdba9077af8f28869dcc1250f24baf
luizaantolini <luizaantolini@gmail.com>
2026-04-01 08:48:31 -0300

    Verificação de maioridade

---
+++ ex02/script.js
@@ -0,0 +1,8 @@
+let idade = Number(prompt("Por favor, informe sua idade"));
+
+if (idade >= 18) {
+    alert("Você é maior de idade");
+} 
+else if (idade < 18) {
+    alert("Você é menor de idade");
+}
