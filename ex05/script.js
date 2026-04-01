4986e1ba0cd5ead0319c0859fcf48af6ef46d606
luizaantolini <luizaantolini@gmail.com>
2026-04-01 09:57:56 -0300

    Múltiplos de cinco

---
+++ ex05/script.js
@@ -0,0 +1,7 @@
+let numero = Number(prompt("Por favor, digite um número"));
+
+    if (numero % 5 === 0) {
+    alert("O número é mútliplo de 5");
+} else {
+    alert("O número não é múltiplo de 5");
+}
