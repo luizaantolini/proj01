abd58aec334ba1d967e7999f0be257d0fb739bf5
luizaantolini <luizaantolini@gmail.com>
2026-04-01 08:38:53 -0300

    Número maior ou menor que zero

---
+++ ex01/script.js
@@ -0,0 +1,9 @@
+let numero = Number(prompt("Por favor, digite um número"));
+
+if (numero > 0) {
+    alert("Número positivo");
+} else if (numero < 0) {
+    alert("Número negativo");
+} else {
+    alert("O número é zero");
+}
